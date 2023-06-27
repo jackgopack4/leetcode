6d300ebd02b245b369cab18564d2df97f9d0c8aa
John Peterson <jlpeterson@utexas.edu>
2023-06-27 14:55:58 -0400

    Create 1480.java

---
+++ 1480.java
@@ -0,0 +1,14 @@
+// 1480. Running Sum of 1d Array
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+// Return the running sum of nums.
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int cur = 0;
+        for(int i=0;i<nums.length;i++) {
+            cur += nums[i];
+            nums[i] = cur;
+        }
+        return nums;
+    }
+}
