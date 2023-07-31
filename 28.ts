d265d4a2f1324c0d21c78ad8e1f59bbdf0a542cb
John Peterson <jlpeterson@utexas.edu>
2023-07-30 22:47:11 -0400

    Create 28.ts

---
+++ 28.ts
@@ -0,0 +1,33 @@
+// 28. Find the Index of the First Occurrence in a String
+
+// Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, 
+// or -1 if needle is not part of haystack.
+function strStr(haystack: string, needle: string): number {
+    let j = 0;
+    let index = 0;
+    let next = 0;
+    let foundNext = false;
+  for (let i = 0; i < haystack.length; i++) {
+      if (haystack.charAt(i) === needle.charAt(j)) {
+          if(j === 0) {
+              index = i;
+          }
+          if(haystack.charAt(i) === haystack.charAt(index) && !foundNext) {
+              foundNext = true;
+              next = i;
+          }
+          j++;
+          if (j === needle.length) {
+              return index;
+          }
+      }
+      else {
+          j = 0;
+          if(foundNext) {
+              i = next;
+              foundNext = false;
+          }
+      }
+  }
+  return -1;
+};
