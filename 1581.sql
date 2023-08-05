c32b82efd8701992c3a86d5ee35af9fd675f205b
John Peterson <jlpeterson@utexas.edu>
2023-08-05 14:44:28 -0400

    Create 1581.sql

---
+++ 1581.sql
@@ -0,0 +1,5 @@
+-- 1581. Customer Who Visited but Did Not Make any Transactions
+-- Write your MySQL query statement below
+select Visits.customer_id, count(Visits.customer_id) as count_no_trans from Visits
+left join Transactions on Visits.visit_id = Transactions.visit_id  where Transactions.visit_id is null
+group by Visits.customer_id;
