33332388db257bcac211dd1029a35c17aab1df00
John Peterson <jlpeterson@utexas.edu>
2023-08-05 14:27:43 -0400

    Create 1378.sql

---
+++ 1378.sql
@@ -0,0 +1,4 @@
+-- 1378. Replace Employee ID With The Unique Identifier
+-- Write your MySQL query statement below
+select EmployeeUNI.unique_id, Employees.name from Employees
+left join EmployeeUNI on Employees.id= EmployeeUNI.id;
