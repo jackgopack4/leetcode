319d494e5c872eee172fd79077f6591a9c090b41
John Peterson <jlpeterson@utexas.edu>
2023-09-17 22:09:34 -0400

    Update 88.py

--- 88.py
+++ 88.py
@@ -1,23 +1,19 @@
-# 88. Merge Sorted Array
-
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        cur1 = m-1
-        cur2 = n-1
-        mergedIndex = m+n-1
-
-        while cur2 >= 0:
-            if cur1 >= 0 and nums2[cur2] < nums1[cur1]:
-                nums1[mergedIndex] = nums1[cur1]
-                cur1-=1
+        # start from back of nums1, either move n1 or insert n2
+        # starting length of nums1 actually m+n
+        n1 = m-1
+        n2 = n-1
+        new_index = m+n-1
+        
+        while nums2:
+            if n1 >= 0 and nums2[-1] < nums1[n1]:
+                nums1[new_index] = nums1[n1]
+                n1 -= 1
             else:
-                nums1[mergedIndex] = nums2[cur2]
-                cur2 -= 1
-            mergedIndex -=1
+                nums1[new_index] = nums2[-1]
+                nums2.pop()
+            new_index -= 1
