27d30fdaf8b405655908ad22a362b807e02fb2f7
John Peterson <jlpeterson@utexas.edu>
2023-09-20 19:49:07 -0400

    Update 344.py

--- 344.py
+++ 344.py
@@ -8,11 +8,7 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        i = 0;
-        j = len(s)-1;
-        while i < j:
-            tmp = s[i];
-            s[i] = s[j];
-            s[j] = tmp;
-            i+=1;
-            j-=1;
+        max = (len(s)) // 2
+        for i in range(max):
+            s[i],s[len(s)-1-i] = s[len(s)-1-i],s[i]
+        return s
