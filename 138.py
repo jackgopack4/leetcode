49903291e694aad710c29f69d1182773b82cf120
John Peterson <jlpeterson@utexas.edu>
2023-09-22 19:39:11 -0400

    Update 138.py

--- 138.py
+++ 138.py
@@ -11,27 +11,15 @@ class Solution:
         if not head:
             return None
         node_dict = {}
-        new_node_list = []
         cur = head
-        count = 0
         while cur:
-            node_dict[cur] = count
-            new_node_list.append(Node(cur.val))
+            node_dict[cur] = Node(cur.val)
             cur = cur.next
-            count += 1
-        # length of list now in count
-        for i in range(len(new_node_list)-1):
-            new_node_list[i].next = new_node_list[i+1]
         cur = head
-        count = 0
-        idx = -1
         while cur:
             if cur.random:
-                idx = node_dict[cur.random]
-            else:
-                idx = -1
-            if idx >= 0:
-                new_node_list[count].random = new_node_list[idx]
+                node_dict[cur].random = node_dict[cur.random]
+            if cur.next:
+                node_dict[cur].next = node_dict[cur.next]
             cur = cur.next
-            count += 1
-        return new_node_list[0]
+        return node_dict[head]
