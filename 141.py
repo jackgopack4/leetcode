eeead1c79a5368040a3321a384efe9a38f83ad98
John Peterson <jlpeterson@utexas.edu>
2023-09-22 19:42:31 -0400

    Create 141.py

---
+++ 141.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = set()
+        cur = head
+        while cur:
+            if cur in seen:
+                seen.clear()
+                return True
+            seen.add(cur)
+            cur = cur.next
+        seen.clear()
+        return False
