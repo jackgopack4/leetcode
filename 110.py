277b7a7348528ed1a66057b587de3eb5b681c415
John Peterson <jlpeterson@utexas.edu>
2023-09-22 21:47:31 -0400

    Create 110.py

---
+++ 110.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def helper(root):
+            if not root:
+                return (True, -1)
+            left_balanced, left_height = helper(root.left)
+            if not left_balanced:
+                return (False, 0)
+            right_balanced, right_height = helper(root.right)
+            if not right_balanced:
+                return (False, 0)
+            return (abs(left_height-right_height) <= 1, 1+max(left_height,right_height))
+        balanced, height = helper(root)
+        return balanced
