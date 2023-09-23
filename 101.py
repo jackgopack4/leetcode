ecb2e0accd42f16304fe83653b28b4430d06d1f1
John Peterson <jlpeterson@utexas.edu>
2023-09-23 15:03:33 -0400

    Create 101.py

---
+++ 101.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def helper(left,right):
+            if not left and not right:
+                return True
+            elif not left or not right:
+                return False
+            return (left.val == right.val) and helper(left.left, right.right) and helper(left.right, right.left)
+        if not root:
+            return True
+        elif not root.left and not root.right:
+            return True
+        return helper(root.left,root.right)
+        
