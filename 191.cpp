785b726d19e90fb7e54901f9cab7e07c2463e27e
John Peterson <jlpeterson@utexas.edu>
2023-10-06 08:45:01 -0500

    Create 191.cpp

---
+++ 191.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res{0};
+        while(n) {
+            n &= (n-1);
+            ++res;
+        }
+        return res;
+    }
+};
