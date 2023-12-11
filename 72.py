7b7a58af7cb919fa3395a31cb5f8a55bd5b5fa11
John Peterson <jlpeterson@utexas.edu>
2023-12-10 19:55:19 -0500

    Update 72.py

--- 72.py
+++ 72.py
@@ -1,20 +1,18 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp = {}
-        def helper(w1idx, w2idx):
-            if w1idx == 0:
-                return w2idx
-            if w2idx == 0:
-                return w1idx
-            if dp.get((w1idx,w2idx)) is not None:
-                return dp[(w1idx,w2idx)]
-            if word1[w1idx-1] == word2[w2idx-1]:
-                return helper(w1idx-1,w2idx-1)
-            min_dist = min(
-                helper(w1idx,w2idx-1),
-                helper(w1idx-1,w2idx),
-                helper(w1idx-1,w2idx-1)
-            )+1
-            dp[(w1idx,w2idx)] = min_dist
-            return min_dist
-        return helper(len(word1),len(word2))
+        dp = [[0]*(len(word2)+1) for _ in range(len(word1)+1)]
+        for w2idx in range(1,len(dp[0])):
+            dp[0][w2idx] = w2idx
+        for w1idx in range(1,len(dp)):
+            dp[w1idx][0] = w1idx
+        for w1idx in range(1,len(dp)):
+            for w2idx in range(1,len(dp[0])):
+                if word1[w1idx-1] == word2[w2idx-1]:
+                    dp[w1idx][w2idx] = dp[w1idx-1][w2idx-1]
+                else:
+                    dp[w1idx][w2idx] = min(
+                        dp[w1idx-1][w2idx],
+                        dp[w1idx][w2idx-1],
+                        dp[w1idx-1][w2idx-1]
+                    )+1
+        return dp[len(word1)][len(word2)]
