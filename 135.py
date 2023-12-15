27e134aea1fae772c1691ea1343023e2f6de920a
John Peterson <jlpeterson@utexas.edu>
2023-12-15 11:58:21 -0500

    Update 135.py

--- 135.py
+++ 135.py
@@ -1,24 +1,13 @@
-# 135. Candy (no more linkedlist, maybe faster?)
-class Solution(object):
-    def candy(self, ratings):
-        """
-        :type ratings: List[int]
-        :rtype: int
-        """
-        length = len(ratings)
-        if length == 1:
-            return 1
-        candies = [1] * length
-        for i in range(length):
-            if i+1 < length:
-                if ratings[i+1] < ratings[i] and candies[i+1] >= candies[i]:
-                    candies[i] = candies[i+1]+1
-                if ratings[i+1] > ratings[i] and candies[i+1] <= candies[i]:
-                    candies[i+1] = candies[i]+1
-        for i in range(length-1,-1,-1):
-            if i-1 >= 0:
-                if ratings[i-1] < ratings[i] and candies[i-1] >= candies[i]:
-                    candies[i] = candies[i-1]+1
-                if ratings[i-1] > ratings[i] and candies[i-1] <= candies[i]:
-                    candies[i-1] = candies[i]+1
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        candies = [1]*len(ratings)
+
+        for i in range(1,len(ratings)):
+            if ratings[i] > ratings[i-1] and candies[i] <= candies[i-1]:
+                candies[i] = candies[i-1]+1
+        
+        for i in range(len(ratings)-2,-1,-1):
+            if ratings[i] > ratings[i+1] and candies[i] <= candies[i+1]:
+                candies[i] = candies[i+1]+1
+
         return sum(candies)
