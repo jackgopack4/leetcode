afef5099b08a167c813cb4b6326dcffa97d7014d
John Peterson <jlpeterson@utexas.edu>
2023-12-15 11:24:09 -0500

    Update 134.py

--- 134.py
+++ 134.py
@@ -4,22 +4,11 @@ class Solution:
         if sum(gas) - sum(cost) < 0:
             return -1
         diff = [g-c for g,c in zip(gas,cost)]
-        max_diffs = sorted([(v,i) for i,v in enumerate(diff) if v >= 0],reverse=True)
-        max_idx = max_diffs[0][1]
-        for _,i in max_diffs:
-            visited = 0
-            cur_gas = 0
-            idx = i
-            while visited < len(gas):
-                cur_gas += gas[idx]
-                cur_gas -= cost[idx]
-                if cur_gas < 0:
-                    break
-                visited += 1
-                if idx < len(gas)-1:
-                    idx += 1
-                else:
-                    idx = 0
-            if visited == len(gas):
-                return i
-        return -1
+        total_diff = 0
+        curr_start = 0
+        for i in range(len(diff)):
+            total_diff += diff[i]
+            if total_diff < 0:
+                total_diff = 0
+                curr_start = i+1
+        return curr_start
